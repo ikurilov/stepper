3df714f0a4412b3937e1f5bc2710cc552edf8bfe
ikurilov <adskiyyo@mail.ru>
2018-09-03 08:54:12 +0300

    fixed name of project;

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'polis'`, async(() => {
+  it(`should have as title 'stepper'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('polis');
+    expect(app.title).toEqual('stepper');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to polis!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to stepper!');
   }));
 });
